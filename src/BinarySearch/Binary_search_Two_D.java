7c97dda723d23a57557ed6196e84f4c31b014b15
L-25 <lokesh.kumarscce2020@sece.ac.in>
2023-09-03 20:24:05 +0530

    Initial commit

--- src/BinarySearch/Binary_search_Two_D.java
+++ src/BinarySearch/Binary_search_Two_D.java
@@ -20,6 +20,7 @@ public class Binary_search_Two_D {
         }
         return new int[]{-1,-1};
     }
+
     public static void main(String[] args) {
         int arr1[][]={{1,2,3},{4,5,6},{7,8,9}};
         int target=3;
